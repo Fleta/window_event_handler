70cbb6479afb843617f179fa70fd4f201820e1a9
Fleta <yasoueisin@gmail.com>
2019-01-14 10:02:11 +0900

    add: get window number by title and change foreground window

--- runner.py
+++ runner.py
@@ -1,20 +1,24 @@
 from time import sleep
 from keyEventHandler import pressHoldRelease
+import window_manager
 
 VIDEO_SHOW_TIME = 180
 MEASURING_INSTRUEMENT_SHOW_TIME = 60
 
-def loop(flag):
+def loop(video_window, measuring_instrument_window, flag):
     if(flag):
         sleep(VIDEO_SHOW_TIME)
+        measuring_instrument_window.show_window()
     else:
         sleep(MEASURING_INSTRUEMENT_SHOW_TIME)
-    pressHoldRelease('alt', 'tab')
+        video_window.show_window()
+    # pressHoldRelease('alt', 'tab')
+    
 
 def setup():
     flag = True
+    video_window = window_manager.WindowMgr().find_window_wildcard('.**')
+    measuring_instruemnt_window = window_manager.WindowMgr().find_window_wildcard('.*Chrome*')
     while(True):
-        loop(flag)
+        loop(video_window, measuring_instruemnt_window, flag)
         flag = not flag
-
-setup()
